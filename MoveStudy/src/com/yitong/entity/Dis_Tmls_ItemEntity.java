9f96bae942b9a73153c05f7fe0320c672479c04b
maojie <maojie@maojie-PC>
2015-06-14 19:00:35 +0800

    合并丢失1

---
+++ MoveStudy/src/com/yitong/entity/Dis_Tmls_ItemEntity.java
@@ -0,0 +1,36 @@
+package com.yitong.entity;
+
+import android.R.integer;
+import android.graphics.drawable.Drawable;
+
+public class Dis_Tmls_ItemEntity {
+
+	public String getPctureurl() {
+		return pctureurl;
+	}
+	public void setPctureurl(String pctureurl) {
+		this.pctureurl = pctureurl;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getNeed() {
+		return need;
+	}
+	public void setNeed(String need) {
+		this.need = need;
+	}
+	String pctureurl;
+	String name;
+	int grade;
+	String need;
+}
